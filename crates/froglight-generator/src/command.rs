a22599fecf16e6c6150b1ccb0ff78c496fa13092
EightFactorial <29801334+EightFactorial@users.noreply.github.com>
2024-03-04 12:32:09 -0800

    Fix warning on empty README

--- crates/froglight-generator/src/command.rs
+++ crates/froglight-generator/src/command.rs
@@ -4,7 +4,7 @@ use clap::Parser;
 
 #[derive(Debug, Clone, PartialEq, Eq, Parser)]
 pub(crate) struct GeneratorArgs {
-    /// The root directory of the FrogLight project
+    /// The root directory of the `FrogLight` project
     ///
     /// From inside the tools submodule, this should be "../"
     #[arg(short = 'd', long = "dir")]
