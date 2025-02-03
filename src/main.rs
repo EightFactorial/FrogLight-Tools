5a5b3e83867439e152b44cd17f4966caea274625
EightFactorial <29801334+EightFactorial@users.noreply.github.com>
2025-02-02 22:17:50 -0800

    Move `main` to the extract crate, build `tools` as an addon
    The point of providing an `main` function and using `directory` is so people can re-use the main function. Simply deriving the module trait on structs automatically adds them to the tool, no code required.
    
    Other projects using this as a base *likely* don't want to use Froglight-specific codegen, so the codegen can be written as it's own extension.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,10 @@
 #![doc = include_str!("../README.md")]
 #![cfg_attr(docsrs, feature(doc_auto_cfg))]
 
-use froglight_tools::main;
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    #[expect(clippy::let_unit_value)]
+    let _data = froglight_extract::extract().await?;
+
+    Ok(())
+}
