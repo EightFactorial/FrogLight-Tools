891d410e411620214e2cc0a28d4598545a7626ee
EightFactorial <29801334+EightFactorial@users.noreply.github.com>
2025-02-09 18:33:26 -0800

    Clean up BlockAttribute enums
    `trace` print all attributes  instead of `debug`

--- src/module/block/attribute/mod.rs
+++ src/module/block/attribute/mod.rs
@@ -1,8 +1,9 @@
 #![allow(clippy::module_inception, dead_code)]
 
-use std::collections::HashSet;
+use std::{collections::HashSet, ops::RangeInclusive, sync::Arc};
 
 use attribute::BlockAttributeAttribute;
+use convert_case::{Case, Casing};
 use froglight_dependency::{
     container::{Dependency, DependencyContainer},
     version::Version,
@@ -17,9 +18,9 @@ pub(crate) use report::BlockReports;
 
 use crate::ToolConfig;
 
-#[derive(Dependency)]
+#[derive(Clone, PartialEq, Eq, Dependency)]
 #[dep(retrieve = BlockAttributes::generate)]
-pub(crate) struct BlockAttributes(pub HashSet<BlockAttributeAttribute>);
+pub(crate) struct BlockAttributes(pub Arc<HashSet<BlockAttributeAttribute>>);
 
 impl BlockAttributes {
     // TODO: Create an enum representation that can get retrieved from
@@ -42,6 +43,79 @@ impl BlockAttributes {
         )
         .await?;
 
-        Ok(Self(attributes))
+        Ok(Self(Arc::new(attributes)))
+    }
+
+    pub(crate) fn as_enum_name(&self, attr: &BlockAttributeAttribute) -> String {
+        self.as_enum_name_internal(attr, &EnumType::from_attribute(attr))
+    }
+
+    fn as_enum_name_internal(
+        &self,
+        attr: &BlockAttributeAttribute,
+        enum_type: &EnumType,
+    ) -> String {
+        match enum_type {
+            EnumType::Bool => format!("{}Bool", attr.name.to_case(Case::Pascal)),
+            EnumType::IntRange(range) => {
+                format!("{}Int{}To{}", attr.name.to_case(Case::Pascal), range.start(), range.end())
+            }
+            EnumType::Enum(values) => {
+                if self.0.iter().filter(|a| a.name == attr.name).count() == 1 {
+                    format!("{}Enum", attr.name.to_case(Case::Pascal))
+                } else {
+                    format!(
+                        "{}Enum_{}",
+                        attr.name.to_case(Case::Pascal),
+                        values
+                            .iter()
+                            .map(|v| v.to_case(Case::Pascal))
+                            .collect::<Vec<_>>()
+                            .join("_"),
+                    )
+                }
+            }
+        }
+    }
+
+    pub(crate) fn as_enum(&self, attr: &BlockAttributeAttribute) -> String {
+        let enum_type = EnumType::from_attribute(attr);
+        let ident = self.as_enum_name_internal(attr, &enum_type);
+
+        match enum_type {
+            EnumType::Bool => format!("pub enum {ident} {{ True, False }}"),
+            EnumType::IntRange(range) => {
+                format!(
+                    "pub enum {ident} {{ {} }}",
+                    range.into_iter().map(|i| format!("_{i}")).collect::<Vec<_>>().join(", ")
+                )
+            }
+            EnumType::Enum(values) => {
+                format!(
+                    "pub enum {ident} {{ {} }}",
+                    values.iter().map(|v| v.to_case(Case::Pascal)).collect::<Vec<_>>().join(", ")
+                )
+            }
+        }
+    }
+}
+
+enum EnumType<'a> {
+    Bool,
+    IntRange(RangeInclusive<u8>),
+    Enum(&'a [String]),
+}
+
+impl<'a> EnumType<'a> {
+    fn from_attribute(attr: &'a BlockAttributeAttribute) -> Self {
+        if attr.values.len() == 2 && attr.values[0..2] == ["true", "false"] {
+            Self::Bool
+        } else if let Ok(integers) =
+            attr.values.iter().map(|v| v.parse::<u8>()).collect::<Result<Vec<_>, _>>()
+        {
+            Self::IntRange(*integers.iter().min().unwrap()..=*integers.iter().max().unwrap())
+        } else {
+            Self::Enum(attr.values.as_slice())
+        }
     }
 }
--- src/module/block/mod.rs
+++ src/module/block/mod.rs
@@ -2,6 +2,7 @@
 
 use froglight_dependency::{container::DependencyContainer, version::Version};
 use froglight_extract::module::ExtractModule;
+use tracing::Level;
 
 mod attribute;
 pub(crate) use attribute::{BlockAttributes, BlockReports};
@@ -15,18 +16,25 @@ pub(crate) struct Blocks;
 
 impl Blocks {
     async fn generate(_version: &Version, deps: &mut DependencyContainer) -> anyhow::Result<()> {
-        let attrs = deps.get_or_retrieve::<BlockAttributes>().await?;
+        let attrs = deps.get_or_retrieve::<BlockAttributes>().await?.clone();
 
-        let mut attrs: Vec<_> = attrs.0.iter().collect();
-        attrs.sort_unstable_by(|a, b| match a.name.cmp(&b.name) {
+        if tracing::enabled!(Level::TRACE) {
+            Self::trace_attributes(&attrs);
+        }
+
+        Ok(())
+    }
+
+    #[allow(dead_code)]
+    fn trace_attributes(attrs: &BlockAttributes) {
+        let mut sorted: Vec<_> = attrs.0.iter().collect();
+        sorted.sort_unstable_by(|a, b| match a.name.cmp(&b.name) {
             std::cmp::Ordering::Equal => a.values.cmp(&b.values),
             other => other,
         });
 
-        for attr in attrs {
-            tracing::info!("Attribute: {attr:?}");
+        for attr in sorted {
+            tracing::trace!("Found {}", attrs.as_enum(attr));
         }
-
-        Ok(())
     }
 }
